fd42ca32298901da24401bcd1e79ce033614d436
MoekaKakimoto <k.moeka0984@gmail.com>
2022-11-10 13:30:33 +0000

    [Update]3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
   def new
+    @Book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(book.id)
+
   def index
+    @books = book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+  
+   def destroy
+    book = book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#top'
   get 'books/new'
   post 'books' => 'books#create'
   get 'books/index'
